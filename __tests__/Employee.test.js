208a6db4ce0f95e4746a6179a64ea307595075c1
Niraj Limbu <nlimbu@yahoo.com>
2022-01-14 16:34:36 -0500

    create employee object

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require('../lib/Employee');
+
+test('creates new employee object', () => {
+  const employee = new Employee('Niraj', 0, 'niraj@yhoo.com');
+
+  expect(typeof employee).toBe('object');
+});
+
+test('get employee name', () => {
+  const employee = new Employee('Niraj', 0, 'niraj@yahoo.com');
+
+  expect(employee.name).toBe('Niraj');
+});
+
+test('creates employee id', () => {
+  const employee = new Employee('Niraj', 0, 'niraj@yahoo.com');
+
+  expect(employee.id).toEqual(expect.any(Number));
+});
+
+test('get employee email', () => {
+  const employee = new Employee('Niraj', 0, 'niraj@yahoo.com');
+
+  expect(employee.email).toBe('niraj@yahoo.com');
+});
+
+test('creates employee role', () => {
+  const employee = new Employee();
+
+  expect(employee.getRole()).toBe('Employee');
+});
