e66d886dd101c4c3a7a4f5555ad141b9da4e950d
Niraj Limbu <nlimbu@yahoo.com>
2022-01-16 16:08:33 -0500

    update index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const jest = require('jest');
 
 // Constructors
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
